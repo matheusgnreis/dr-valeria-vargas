84d375dab2e63de341bff68697798f29c348de13
matheusgnreis <matheus@e-com.club>
2023-08-25 14:31:55 -0300

    chore: first step

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,10 @@
 // Add your custom JavaScript for storefront pages here.
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
